c1515adaf8f3a202385d9f75201208cec5f64c7d
Kyle Siopiolosz <138803045+ksiopiolosz-aterlo@users.noreply.github.com>
2025-10-27 20:19:12 +0000

    Modularize starter code for better organization

--- src/main.rs
+++ src/main.rs
@@ -3,96 +3,12 @@ extern crate rocket;
 
 use log::info;
 use rocket::fairing::AdHoc;
-use rocket::http::Status;
-use rocket::serde::{json::Json, Deserialize};
-use serde::Serialize;
-use serde_json::Value;
-use std::collections::HashMap;
 use std::env;
 
+mod bot;
 mod config;
-mod logic;
-
-// API and Response Objects
-// See https://docs.battlesnake.com/api
-
-#[derive(Deserialize, Serialize, Debug)]
-pub struct Game {
-    id: String,
-    ruleset: HashMap<String, Value>,
-    timeout: u32,
-}
-
-#[derive(Deserialize, Serialize, Debug)]
-pub struct Board {
-    height: u32,
-    width: i32,
-    food: Vec<Coord>,
-    snakes: Vec<Battlesnake>,
-    hazards: Vec<Coord>,
-}
-
-#[derive(Deserialize, Serialize, Debug)]
-pub struct Battlesnake {
-    id: String,
-    name: String,
-    health: i32,
-    body: Vec<Coord>,
-    head: Coord,
-    length: i32,
-    latency: String,
-    shout: Option<String>,
-}
-
-#[derive(Deserialize, Serialize, Debug)]
-pub struct Coord {
-    x: i32,
-    y: i32,
-}
-
-#[derive(Deserialize, Serialize, Debug)]
-pub struct GameState {
-    game: Game,
-    turn: i32,
-    board: Board,
-    you: Battlesnake,
-}
-
-#[get("/")]
-fn handle_index() -> Json<Value> {
-    Json(logic::info())
-}
-
-#[post("/start", format = "json", data = "<start_req>")]
-fn handle_start(start_req: Json<GameState>) -> Status {
-    logic::start(
-        &start_req.game,
-        &start_req.turn,
-        &start_req.board,
-        &start_req.you,
-    );
-
-    Status::Ok
-}
-
-#[post("/move", format = "json", data = "<move_req>")]
-fn handle_move(move_req: Json<GameState>) -> Json<Value> {
-    let response = logic::get_move(
-        &move_req.game,
-        &move_req.turn,
-        &move_req.board,
-        &move_req.you,
-    );
-
-    Json(response)
-}
-
-#[post("/end", format = "json", data = "<end_req>")]
-fn handle_end(end_req: Json<GameState>) -> Status {
-    logic::end(&end_req.game, &end_req.turn, &end_req.board, &end_req.you);
-
-    Status::Ok
-}
+mod handler;
+mod types;
 
 #[launch]
 fn rocket() -> _ {
@@ -113,7 +29,12 @@ fn rocket() -> _ {
 
     info!("Starting Battlesnake Server...");
 
+    // Load configuration once at startup
+    let config = config::Config::load_or_default();
+    let bot = bot::Bot::new(config);
+
     rocket::build()
+        .manage(bot)
         .attach(AdHoc::on_response("Server ID Middleware", |_, res| {
             Box::pin(async move {
                 res.set_raw_header("Server", "battlesnake/github/starter-snake-rust");
@@ -121,6 +42,6 @@ fn rocket() -> _ {
         }))
         .mount(
             "/",
-            routes![handle_index, handle_start, handle_move, handle_end],
+            routes![handler::index, handler::start, handler::get_move, handler::end],
         )
 }
